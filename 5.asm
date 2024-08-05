81f44a0ac431bcf8746f9b7ae0ed4d97a7a83f4c
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2024-08-05 13:07:10 +0500

    iterations in assembly

--- 5.asm
+++ 5.asm
@@ -1,16 +1,17 @@
-[org 0x0100]
+[org 0x0100]            ; Set the origin to 0x0100 for COM file format
 
-mov bx,num1
-mov cx,10
-xor ax,ax
+mov bx, num1            ; Load the address of num1 into BX
+mov cx, 10              ; Load the value 10 into CX (loop counter)
+xor ax, ax              ; Clear AX (set AX to 0)
+
 iteration:
-add ax,[bx]
-add bx,2
-sub cx,1
-jnz iteration
+add ax, [bx]            ; Add the value at memory location pointed to by BX to AX
+add bx, 2               ; Increment BX to point to the next word (2 bytes)
+sub cx, 1               ; Decrement CX by 1
+jnz iteration           ; Jump to iteration if CX is not zero (loop until CX is 0)
 
-mov [num1+20],ax
-mov ax,0x4c00
-int 0x21
+mov [num1+20], ax       ; Store the result (sum of values) into the memory location num1+20 (after the last number)
+mov ax, 0x4c00          ; Load 0x4c00 into AX (function 4Ch of DOS interrupt to terminate program)
+int 0x21                ; Interrupt 21h to call DOS function (terminate program)
 
-num1 : dw 5,10,15,20,25,30,35,40,45,50,0
+num1 : dw 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 0  ; Define word data at label num1 with 10 values and an extra 0
