631e1a33b8fc9763cea3a3680dfb73a46e2fe013
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2024-08-05 13:04:13 +0500

    addressing

--- 3.asm
+++ 3.asm
@@ -1,12 +1,13 @@
-[org 0x0100]
+[org 0x0100]        ; Set the origin to 0x0100 for COM file format
 
-mov ax,[num1]
-mov bx,[num1+2]
-add ax,bx
-mov bx,[num1+4]
-add ax,bx
-mov [num1+6],ax
-mov ax,0x4c00
-int 0x21
+mov ax, [num1]      ; Load the value at memory location num1 into AX (num1 = 5)
+mov bx, [num1+2]    ; Load the value at memory location num1+2 into BX (num1+2 = 10)
+add ax, bx          ; Add the value in BX (10) to AX (5), result is 15
+mov bx, [num1+4]    ; Load the value at memory location num1+4 into BX (num1+4 = 15)
+add ax, bx          ; Add the value in BX (15) to AX (15), result is 30
+mov [num1+6], ax    ; Store the result (30) into memory location num1+6
 
-num1 : dw 5,10,15,0
+mov ax, 0x4c00      ; Load 0x4c00 into AX (function 4Ch of DOS interrupt to terminate program)
+int 0x21            ; Interrupt 21h to call DOS function (terminate program)
+
+num1 : dw 5, 10, 15, 0 ; Define word data at label num1 with values 5, 10, 15, and 0
